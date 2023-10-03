7496326d43a90cf0c1ae6cefa7fc6b4e3b84fadf
doofmars <git@preuss.in>
2023-10-03 12:10:29 +0200

    Downgrade to 3.5.2 lts version

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ssp-online
 A simple rps-online clone in godot
 
-Godot version: 4.1.1
+Godot version: 3.5.2
+Rational: This version is supporting iOS devices in web export and has overall less issues caused by SharedArrayBuffers.
 
 Video for reference: https://www.youtube.com/watch?v=Q2sDtTbzHiI
--- src/Level.gd
+++ src/Level.gd
@@ -4,7 +4,7 @@ var SQUARE_SIZE = 80
 var SQUARE_COUNT_X = 7
 var SQUARE_COUNT_Y = 6
 var BOARD_POS_X = 100
-var BOARD_POS_Y = (ProjectSettings.get_setting("display/window/size/viewport_height") - SQUARE_SIZE * SQUARE_COUNT_Y) / 2
+var BOARD_POS_Y = (600 - SQUARE_SIZE * SQUARE_COUNT_Y) / 2
 var BORDER_COLOR = "c6d878"
 var BORDER_WIDTH = 2
 var SQUARE_COLOR_1 = "c6d878"
@@ -19,16 +19,17 @@ var gameState = "PlaceFlag"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var border = ColorRect.new()
-	border.size = Vector2(SQUARE_SIZE * SQUARE_COUNT_X + BORDER_WIDTH*2, SQUARE_SIZE * SQUARE_COUNT_Y + BORDER_WIDTH*2)
-	border.position = Vector2(BOARD_POS_X - BORDER_WIDTH, BOARD_POS_Y - BORDER_WIDTH)
-	print(ProjectSettings.get_setting("display/window/size/viewport_height"))
+	print(SQUARE_SIZE * SQUARE_COUNT_X + BORDER_WIDTH*2)
+	border.rect_size = Vector2(float(SQUARE_SIZE * SQUARE_COUNT_X + BORDER_WIDTH*2), float(SQUARE_SIZE * SQUARE_COUNT_Y + BORDER_WIDTH*2))
+	border.rect_position = Vector2(BOARD_POS_X - BORDER_WIDTH, BOARD_POS_Y - BORDER_WIDTH)
+
 	border.color = BORDER_COLOR
 	add_child(border)
 	for x in SQUARE_COUNT_X:
 		for y in SQUARE_COUNT_Y:
 			var square = ColorRect.new()
-			square.position = Vector2(BOARD_POS_X + x * SQUARE_SIZE, BOARD_POS_Y + y * SQUARE_SIZE)
-			square.size = Vector2(SQUARE_SIZE, SQUARE_SIZE)
+			square.rect_position = Vector2(BOARD_POS_X + x * SQUARE_SIZE, BOARD_POS_Y + y * SQUARE_SIZE)
+			square.rect_size = Vector2(SQUARE_SIZE, SQUARE_SIZE)
 			square.name = str("square", x, "-", y)
 			#print(str("(", BOARD_POS_X + x * SQUARE_SIZE, ", ", BOARD_POS_Y + y * SQUARE_SIZE, " -> ", (x % 2 + y % 2) % 2 == 0, ")"))
 			if (x % 2 + y % 2) % 2 == 0:
@@ -51,7 +52,6 @@ func set_random_item(pawn):
 	var state = states[randi() % states.size()]
 	pawn.show_item(state)
 
-
 func _on_menu_start_singleplayer():
 	var GameUI = get_node("GameUI")
 	GameUI.show()
@@ -61,18 +61,18 @@ func _on_menu_start_singleplayer():
 	# Place enemy pawns
 	for x in SQUARE_COUNT_X:
 		for y in PAWN_ROWS:
-			var pawn_enemy_node = PawnEnemy.instantiate()
+			var pawn_enemy_node = PawnEnemy.instance()
 			pawn_enemy_node.set_name(str("enemy", x, "-", y))
 			pawn_enemy_node.position = Vector2(BOARD_POS_X + x * SQUARE_SIZE, BOARD_POS_Y + y * SQUARE_SIZE)
 			add_child(pawn_enemy_node)
 	# Place my pawns
 	for x in SQUARE_COUNT_X:
 		for y in PAWN_ROWS:
-			var pawn_mine_node = PawnMine.instantiate()
+			var pawn_mine_node = PawnMine.instance()
 			pawn_mine_node.set_name(str("mine", x, "-", y))
 			pawn_mine_node.position = Vector2(BOARD_POS_X + x * SQUARE_SIZE, BOARD_POS_Y + SQUARE_SIZE * SQUARE_COUNT_Y - (y + 1) * SQUARE_SIZE)
 			pawn_mine_node.hide_all()
-			pawn_mine_node.connect("pawn_clicked", _on_pawn_clicked, 0)
+			pawn_mine_node.connect("pawn_clicked", self, "_on_pawn_clicked")
 			add_child(pawn_mine_node)
 
 func _on_pawn_clicked(pawn):
