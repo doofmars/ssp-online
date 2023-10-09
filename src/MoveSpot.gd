b3a40c326b8320616d2d01fb79f473ccd3aa675c
doofmars <git@preuss.in>
2023-10-09 10:21:01 +0200

    Add click handler to MoveSpot Node

--- src/MoveSpot.gd
+++ src/MoveSpot.gd
@@ -7,6 +7,7 @@ var pawn = null
 var posX = -1
 var posY = -1
 var radius = 10
+var color = Color(1, 1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,18 +20,18 @@ func _ready():
 
 func _draw():
 	# Draw a circle
-	draw_circle(Vector2(0, 0), radius, Color(1, 1, 1))
+	draw_circle(Vector2(0, 0), radius, color)
 
-func _on_control_gui_input(event):
+func _on_Control_mouse_entered():
+	color = Color(0, 1, 0)
+	mouse_over = true
+
+func _on_Control_mouse_exited():
+	color = Color(1, 1, 1)
+	mouse_over = false
+
+func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
 		if mouse_over and event.pressed:
 			print(str("Clicked On MoveSpot ", name, " ", Constants.Direction.keys()[direction]))
 			emit_signal("move_spot_clicked", self)
-
-
-func _on_control_mouse_exited():
-	mouse_over = false
-
-
-func _on_control_mouse_entered():
-	mouse_over = true
